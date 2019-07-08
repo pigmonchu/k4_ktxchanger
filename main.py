d6f16b7a2dcf73477c73840510b0075ef48ae82b
Ramón Maldonado Rodríguez <monterdi@gmail.com>
2019-07-08 23:11:16 +0200

    LLamadas con requests

---
+++ main.py
@@ -0,0 +1,30 @@
+from tkinter import *
+from tkinter import ttk
+
+import configparser
+import json
+
+import requests
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+
+inSymbol = input('Qué moneda quieres convertir:')
+outSymbol = input('En qué otra moneda:')
+
+url = config['fixer.io']['RATE_LATEST_EP']
+api_key = config['fixer.io']['API_KEY']
+
+url = url.format(api_key, inSymbol, outSymbol)
+response = requests.get(url)
+if response.status_code == 200:
+    print(response.text)
+else:
+    print("Se ha producido un error en la peticion:", response.status_code)
+
+a = 1
+
+
+
+
